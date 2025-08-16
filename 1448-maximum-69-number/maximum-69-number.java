4becb310de7153cb64e2b8887808843659929ac1
Md Arsul Amin <126336341+Arshul26@users.noreply.github.com>
2025-08-16 19:25:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (56.69%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -0,0 +1,23 @@
+//Approach (Without converting to string)
+//T.C : O(d) , d = number of dogits in num
+//S.C : O(1)
+class Solution {
+    public int maximum69Number (int num) {
+        int place = 0;
+        int index = -1;
+        int temp = num;
+        
+        while (temp > 0) {
+            int remain = temp % 10;
+            if (remain == 6) {
+                index = place;
+            }
+            temp /= 10;
+            place++;
+        }
+        
+        if (index == -1) return num;
+        
+        return num + 3 * (int)Math.pow(10, index);
+    }
+}
